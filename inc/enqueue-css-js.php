03e230e8f1d7777e3e67d71b5a73177f6e8a8729
Michu <info@galliweb.ch>
2025-03-03 14:26:37 +0100

    scss to css fixes

--- inc/enqueue-css-js.php
+++ inc/enqueue-css-js.php
@@ -1,6 +1,5 @@
 <?php
 // Main Stylesheet und Scripts
-// inc/enqueue.php
 function minimal_theme_scripts()
 {
   // Enqueue main stylesheet
@@ -16,14 +15,13 @@ function minimal_theme_scripts()
 add_action('wp_enqueue_scripts', 'minimal_theme_scripts');
 
 // Admin Styles
-// inc/admin-styles.php
 function minimal_theme_admin_styles()
 {
-  $admin_css_path = get_template_directory() . '/cms/css/admin.css';
+  $admin_css_path = get_template_directory() . '/assets/css/admin.css';
   if (file_exists($admin_css_path)) {
     wp_enqueue_style(
       'minimal-theme-admin-styles',
-      get_template_directory_uri() . '/cms/css/admin.css',
+      get_template_directory_uri() . '/assets/css/admin.css',
       array(),
       filemtime($admin_css_path),
       'all'
@@ -34,11 +32,11 @@ add_action('admin_enqueue_scripts', 'minimal_theme_admin_styles');
 
 function minimal_theme_login_styles()
 {
-  $login_css_path = get_template_directory() . '/cms/css/login.css';
+  $login_css_path = get_template_directory() . '/assets/css/login.css';
   if (file_exists($login_css_path)) {
     wp_enqueue_style(
       'minimal-theme-login-styles',
-      get_template_directory_uri() . '/cms/css/login.css',
+      get_template_directory_uri() . '/assets/css/login.css',
       array(),
       filemtime($login_css_path),
       'all'
--- inc/helpers.php
+++ inc/helpers.php
@@ -26,3 +26,51 @@ function minimal_theme_pingback_header()
   }
 }
 add_action('wp_head', 'minimal_theme_pingback_header');
+
+// Add user ID to admin body class
+function add_user_id_to_admin_body_class($classes)
+{
+  // Get the current user ID
+  $current_user_id = get_current_user_id();
+
+  // Append the user ID as a class
+  if ($current_user_id) {
+    $classes .= ' user-id-' . $current_user_id;
+  }
+
+  return $classes;
+}
+add_filter('admin_body_class', 'add_user_id_to_admin_body_class');
+
+
+// user role body class
+function add_user_role_body_class($classes)
+{
+  // Check if the user is logged in
+  if (is_admin() && is_user_logged_in()) {
+    $user = wp_get_current_user();
+    if (!empty($user->roles)) {
+      // Add the first role as a class (WordPress allows multiple roles, usually the first one is primary)
+      $classes .= ' user-role-' . sanitize_html_class($user->roles[0]);
+    }
+  }
+  return $classes;
+}
+add_filter('admin_body_class', 'add_user_role_body_class');
+
+
+// post type class
+
+function add_custom_post_type_body_class($classes)
+{
+  // Check if we're in the block editor
+  if (is_admin() && function_exists('get_current_screen')) {
+    $screen = get_current_screen();
+    if ($screen && $screen->post_type) {
+      // Add the custom post type class
+      $classes .= ' post-type-' . sanitize_html_class($screen->post_type);
+    }
+  }
+  return $classes;
+}
+add_filter('admin_body_class', 'add_custom_post_type_body_class');
