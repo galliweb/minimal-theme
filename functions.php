c89a57c5c1175cc1952a3d7de097c3e96bbd4202
Michu <info@galliweb.ch>
2025-03-20 23:15:22 +0100

    vite setup

--- functions.php
+++ functions.php
@@ -8,20 +8,38 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly
 }
 
-// Main Stylesheet und Scripts
-function starter_theme_scripts()
-{
-  // Enqueue main stylesheet
-  wp_enqueue_style(
-    'fastpage-style',
-    get_template_directory_uri() . '/main.css',
-    array(),
-    filemtime(get_template_directory() . '/main.css')
-  );
 
-  wp_enqueue_script('fastpage-script', get_template_directory_uri() . '/main.js', array(), wp_get_theme()->get('Version'), true);
-}
-add_action('wp_enqueue_scripts', 'starter_theme_scripts');
+// ... other code
+add_action('wp_enqueue_scripts', function(){
+    $manifestPath = get_theme_file_path('dist/.vite/manifest.json');
+
+    // Check if the manifest file exists and is readable before using it
+    if (file_exists($manifestPath)) {
+        $manifest = json_decode(file_get_contents($manifestPath), true);
+ 
+        // Check if the file is in the manifest before enqueuing
+        if (isset($manifest['src/scripts/main.js'])) {
+            wp_enqueue_script('mytheme', get_theme_file_uri('dist/' . $manifest['src/scripts/main.js']['file']));
+        }
+    }
+});
+
+add_action('wp_enqueue_scripts', function(){
+    $manifestPath = get_theme_file_path('dist/.vite/manifest.json');
+ 
+    // Check if the manifest file exists and is readable before using it
+    if (file_exists($manifestPath)) {
+        $manifest = json_decode(file_get_contents($manifestPath), true);
+ 
+        // Check if the file is in the manifest before enqueuing
+        if (isset($manifest['src/scripts/main.js'])) {
+            wp_enqueue_script('mytheme', get_theme_file_uri('dist/' . $manifest['src/scripts/main.js']['file']));
+            // Enqueue the CSS file
+            wp_enqueue_style('mytheme', get_theme_file_uri('dist/' . $manifest['src/scripts/main.js']['css'][0]));
+        }
+    }
+});
+
 
 // Admin Styles
 function starter_theme_admin_styles()
---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+/**
+ * Main JS file for Cartify Child
+ * 
+ * Import the main css file
+ * Import all the JavaScript files that are needed for the theme.
+ */
+
+// Import the main CSS file
+import '../styles/main.scss'
+
+// Build the main JS file
+/*
+import './components/nav.js'; 
+*/
